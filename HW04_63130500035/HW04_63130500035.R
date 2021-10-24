904d7f4bc4c1ad0c088931e596cc4fb4c8e3dab7
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-10-24 17:54:58 +0700

    add

---
+++ HW04_63130500035/HW04_63130500035.R
@@ -0,0 +1,16 @@
+install.packages("dplyr", "readr", "DescTools", "assertive", "stringr", "ggplot2", "DataExplorer", "tidyverse", "forcats",)  
+library(dplyr) #
+library(readr) # 
+
+library(DescTools) 
+library(assertive)
+library(stringr)
+library(ggplot2) #
+
+library(DataExplorer) #
+
+library(tidyverse)                          
+library(forcats)
+
+
+comsci <- read_csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/prog_book.csv")
